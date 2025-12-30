09edb4afe80aeaa8f777d7aa442416024bedc841
Elissandra Oliveira <oliver-lis@hotmail.com>
2025-12-30 01:35:10 -0400

    Criando o projeto

---
+++ src/test/java/pages/CadastroUsuarioPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import runner.RunCucumber;
+import static support.Commands.*;
+
+public class CadastroUsuarioPage extends RunCucumber {
+
+    // elementos
+    private By campoNome = By.id("user");
+    private By campoEmail = By.id("email");
+    private By campoSenha = By.id("password");
+    private By botaoFazerCadastro= By.id("btnRegister");
+
+    // ações / funções / métodos
+    public void preencheNome(String nome){
+        fillDield(campoNome, nome);
+    }
+    public void preencheEmail(String email){
+        fillDield(campoEmail, email);
+    }
+
+    public void preencherSenha(String senha){
+        fillDield(campoSenha, senha);
+    }
+
+    public void cadastrarUsuario(){
+        clickElement(botaoFazerCadastro);
+    }
+
+    public void verificaCadastroSucesso(){
+        checkMessage(By.id("swal2-title"), "Cadastro realizado!");
+    }
+}
---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,53 @@
+package pages;
+
+import org.openqa.selenium.By;
+import runner.RunCucumber;
+import static support.Commands.*;
+
+public class LoginPage extends RunCucumber {
+
+    // elementos
+    private String URL = "http://automationpratice.com.br/";
+    private By botaoAcessarLogin = By.cssSelector(".right_list_fix > li > a > .fa-user");
+    private By campoEmail = By.id("user");
+    private By campoSenha = By.id("password");
+    private By botaoFazerLogin = By.id("btnLogin");
+    private By botaoAcessarCadastro = By.cssSelector(".right_list_fix > li > a > .fa-lock");
+
+    // ações / funções / métodos
+    public void acessarAplicao() {
+        String browser = System.getProperty("browser");
+        getDriver(browser).get(URL);
+    }
+
+    public void acessarTelaLogin() {
+        clickElement(botaoAcessarLogin);
+    }
+
+    public void preencheEmail(String email){
+        fillDield(campoEmail, email);
+    }
+
+    public void preencherSenha(String senha){
+        fillDield(campoSenha, senha);
+    }
+
+    public void clicarLogin(){
+        clickElement(botaoFazerLogin);
+    }
+
+    public void verificaLoginSucesso(){
+        checkMessage(By.id("swal2-title"), "Login realizado");
+        //waitElementBeVisible(By.id("swal2-title"), 10000);
+    }
+
+    public void verificaCampoVazio(String message){
+        checkMessage(By.className("invalid_input"), message);
+        //waitElementBeVisible(By.id("swal2-title"), 10000);
+    }
+
+    public void acessarTelaCadastro(){
+        clickElement(botaoAcessarCadastro);
+    }
+
+}
