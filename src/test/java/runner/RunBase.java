fdbad82352c71859023f6a3095db9c13d13c7e2d
Elissandra Oliveira <oliver-lis@hotmail.com>
2026-01-01 16:32:26 -0400

    Criando notificação para Slack na pipeline

--- src/test/java/runner/RunBase.java
+++ src/test/java/runner/RunBase.java
@@ -46,7 +46,7 @@ public class RunBase {
         }
 
         if(driver != null){
-            driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
             driver.manage().window().maximize();
         }
 
